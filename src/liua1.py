3b4051eb2ae656fa83653ff974b20674d89e693e
aoliu9 <liua1@rose-hulman.edu>
2018-10-03 16:45:11 -0400

    go forward

--- src/liua1.py
+++ src/liua1.py
@@ -12,6 +12,7 @@ def main():
     run_tests()
 
 
+
 def run_tests():
     """ Runs various tests. """
     run_test_go_stop()
@@ -21,22 +22,21 @@ def run_test_go_stop():
     """ Tests the   go   and   stop   Snatch3rRobot methods. """
     robot = rb.Snatch3rRobot()
 
-    robot.go(50, 25)
-    time.sleep(2)
-    robot.stop()
+    #robot.go(50, 25)
+    #time.sleep(2)
+    #robot.stop()
 
-    print(robot.right_wheel.get_degrees_spun())
-    print(robot.left_wheel.get_degrees_spun())
-    robot.left_wheel.reset_degrees_spun(0)
+    #print(robot.right_wheel.get_degrees_spun())
+    #print(robot.left_wheel.get_degrees_spun())
+    #robot.left_wheel.reset_degrees_spun(0)
 
-    time.sleep(2)
+    #time.sleep(2)
 
     robot.go(100, 100)
-    time.sleep(3)
+    #time.sleep(3)
     robot.stop(rb.StopAction.COAST.value)
 
-    print(robot.right_wheel.get_degrees_spun())
-    print(robot.left_wheel.get_degrees_spun())
+
 
 
 def go_forward(left_wheel_duty_cycle_percent, right_wheel_duty_cycle_percent):
